1ad43d63708d3640afc272f887dd43241f454d7b
Max Cohen <cohenmax9@gmail.com>
2014-02-16 15:07:51 -0500

    Background and tree drawn

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,13 @@
-// Your work goes here...
+function draw() {
+	canvas = document.getElementById('game');
+	ctx = canvas.getContext('2d');
+
+	var img = new Image();
+	img.src = "assets/duckhunt.png";
+
+	ctx.drawImage(img, 0, 400, 900, 500, 0, 0, 800, 600);
+	ctx.drawImage(img, 0, 275, 80, 175, 50, 10, 250, 690);
+
+	
+}
+
