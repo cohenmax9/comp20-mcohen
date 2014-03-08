fbbde0c184f9b6ab0ff51cfbe78829d67f9b1b9f
Max Cohen <cohenmax9@gmail.com>
2014-03-07 22:57:34 -0500

    single infowindow, added time to infowindow

--- transit/transit.js
+++ transit/transit.js
@@ -1,4 +1,5 @@
 var map;
+var infowindow;
 
 function userPosition(position) {
    	var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -8,12 +9,10 @@ function userPosition(position) {
        	title: 'Your Location'
    	}
    	var marker = new google.maps.Marker(markerOptions);
-   	var infowindow = new google.maps.InfoWindow({
-    	content: "I am here"
-  	});
 
    	marker.setMap(map);
    	google.maps.event.addListener(marker, 'click', function() {
+   		infowindow.setContent("You are here");
     	infowindow.open(map,marker);
   	});
 }
@@ -31,6 +30,11 @@ function initialize() {
     }
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
+    var infoOptions = {
+    	content: "Hey"
+    };
+
+    infowindow = new google.maps.InfoWindow(infoOptions);
     navigator.geolocation.getCurrentPosition(userPosition, geoLocationError);
 
     getTrainData();   	
@@ -57,6 +61,8 @@ function createMarkers() {
 			dest = scheduleData.schedule[14].Destination;
 			lat = scheduleData.schedule[14].Position.Lat;
 			lng = scheduleData.schedule[14].Position.Long;
+			arrivalTime = scheduleData.schedule[14].Predictions[0].Seconds;
+
 			var stopCoords = new google.maps.LatLng(lat, lng);
 		   	var markerOptions = {
 		   		position: stopCoords,
@@ -65,8 +71,10 @@ function createMarkers() {
 		   	}
 
 		   	var marker = new google.maps.Marker(markerOptions);
-		   	var infowindow = new google.maps.InfoWindow({
-		    	content: dest
+		   	
+		   	google.maps.event.addListener(marker, 'click', function() {
+		    	infowindow.setContent(dest + ": " + secsToMins(arrivalTime, 0) + " until train arrives.");
+		    	infowindow.open(map, this);
 		    });
 
 		   	marker.setMap(map);
@@ -76,8 +84,24 @@ function createMarkers() {
 		//}
    }
    else if (xhr.readyState == 4 && xhr.status == 500) {
-		scheduleDom = document.getElementById("schedule");
-		scheduleDom.innerHTML = "<p>Failure</p>";
+		scheduleDom = document.getElementById("map_canvas");
+		scheduleDom.innerHTML = "<h1>Failure to load trains. Server Error</h1>";
+	}
+}
+
+function secsToMins(secs, mins) {
+	if (secs < 59) {
+		if (secs <= 9) {
+			return mins + ":" + "0" + secs;
+		}
+		else {
+			return mins + ":" + secs;
+		}
+	}
+	else {
+		mins += 1;
+		secs -= 60;
+		return secsToMins(secs, mins);
 	}
 }
 
